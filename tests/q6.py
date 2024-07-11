0c3bcebf0ab0bccad74afd943c7da74b7a105c7e
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-07-11 21:19:51 +0900

    demo

---
+++ tests/q6.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q6',
+    'points': None,
+    'suites': [   {   'cases': [{'code': ">>> assert 'x' in globals(), '変数xが定義されていません'\n>>> assert x == 5, '変数xは5でなければなりません'\n", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
