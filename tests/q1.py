658e72b1547dcb12d56c5476aac136ffc69d9dc6
Akira Yamada <akirayamada@people.kobe-u.ac.jp>
2024-09-02 10:43:18 +0900

    q1.py

--- tests/q1.py
+++ tests/q1.py
@@ -1,7 +1,7 @@
 OK_FORMAT = True
 
 test = {   'name': 'q1',
-    'points': 1,
+    'points': None,
     'suites': [   {   'cases': [{'code': '>>> square(1) == 1\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> square(0) == 0\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
